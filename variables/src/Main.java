18d52e441344d883a79393a867fdb50cdeb7cd23
Onur Nasır <onur.nasr@gmail.com>
2020-08-02 18:13:40 +0300

    Variables example added.

---
+++ variables/src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+//reusability = tekrar tekrar kullanılabilirlik demek aşağıdaki yapı gibi.
+    public static void main(String[] args) {
+	    //Case sensitive = büyük küçük harflere duyarlıdır.
+        int ogrenciSayisi=10;
+        String mesaj="Öğrenci sayısı:";
+        System.out.println(mesaj+ogrenciSayisi);
+        System.out.println("Öğrenci sayım:"+ogrenciSayisi);
+        System.out.println("Öğrenci sayım:9");
+        System.out.println("Öğrenci sayım:9");
+    }
+}
